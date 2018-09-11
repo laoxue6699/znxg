6a59415048fd19baeca6dced5c2ae02326382444
laoxue6699 <grain2010@sina.cn>
2018-09-11 10:04:13 +0800

    首页框架设计完成

--- src/components/bottom.vue
+++ src/components/bottom.vue
@@ -1,22 +1,26 @@
 <template>
     <div>
         <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active touch"  @click="goto('/stoke')">
+			<span class="mui-tab-item mui-active touch"  @click="goto('/home')">
 				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</span>
+			<span class="mui-tab-item touch" @click="goto('/stoke')" >
+				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">库存</span>
-			</a>
-			<a class="mui-tab-item touch" href="#tabbar-with-chat" @click="goto('/sale')">
-				<span class="mui-icon mui-icon-email"></span>
+			</span>
+			<span class="mui-tab-item touch"  @click="goto('/sale')" >
+				<span class="mui-icon mui-icon-map"></span>
 				<span class="mui-tab-label">销售</span>
-			</a>
-			<a class="mui-tab-item touch" href="#tabbar-with-contact" @click="goto('/capital')">
+			</span>
+			<span class="mui-tab-item touch"  @click="goto('/capital')">
 				<span class="mui-icon mui-icon-gear"></span>
 				<span class="mui-tab-label">资金</span>
-			</a>
-			<a class="mui-tab-item touch" href="#tabbar-with-map" @click="goto('/profile')">
+			</span>
+			<span class="mui-tab-item touch" @click="goto('/profile')">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">我</span>
-			</a>
+			</span>
 		</nav>
     </div>
 </template>
@@ -33,4 +37,5 @@
 
 <style scoped>
 	.touch {touch-action: none;}
+	.active {color: #007aff;}
 </style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,60 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <div id="banner">
+            <img src="../../assets/images/slider3.jpg">
+        </div>
+        <div class="mui-content">
+		        <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-home"></span>
+		                    <div class="mui-media-body">Home</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+		                    <div class="mui-media-body">Email</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-chatbubble"></span>
+		                    <div class="mui-media-body">Chat</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-location"></span>
+		                    <div class="mui-media-body">location</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-search"></span>
+		                    <div class="mui-media-body">Search</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-phone"></span>
+		                    <div class="mui-media-body">Phone</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-gear"></span>
+		                    <div class="mui-media-body">Setting</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-info"></span>
+		                    <div class="mui-media-body">about</div></a></li>
+		           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-more"></span>
+		                    <div class="mui-media-body">more</div></a></li>
+                    <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-chatbubble"></span>
+		                    <div class="mui-media-body">Chat</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-location"></span>
+		                    <div class="mui-media-body">location</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-search"></span>
+		                    <div class="mui-media-body">Search</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-phone"></span>
+		                    <div class="mui-media-body">Phone</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-gear"></span>
+		                    <div class="mui-media-body">Setting</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-info"></span>
+		                    <div class="mui-media-body">about</div></a></li>
+		           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-more"></span>
+		                    <div class="mui-media-body">more</div></a></li>
+		        </ul> 
+		</div>
         <bottom></bottom>
     </div>
 </template>
@@ -12,7 +66,11 @@
             bottom
         }
     }
+
 </script>
 
 <style scoped>
+    img {
+        width: 100%;
+    }
 </style>
